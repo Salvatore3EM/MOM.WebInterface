cce0ed3407a159beef2e8638f74d16caddc9c322
massimiliano <massimiliano.murrone@eng.it>
2025-05-28 22:36:38 +0200

    ultime modifiche per il rendering dei tratti

--- MOM.WebInterface/App_DB/DbQueries.cs
+++ MOM.WebInterface/App_DB/DbQueries.cs
@@ -46,12 +46,40 @@ namespace MOM.WebInterface.App_DB
                     //    .SqlQuery<ResultForCampaign>("GetResultsForCampaign @ClientId", clientIdParameter)
                     //    .ToList();
                     var result = context.Database
-                        .SqlQuery<PlantModelTreeDtoBase>("PlantModelTreeMontaggioFrontEnd")
+                        .SqlQuery<PlantModelTreeDtoBase>("GetTreeWS")
                         .ToList();
 
                     return result;
                 }
             }
+
+
+            /// <summary>
+            /// recuper la WorkStation a cui appartiene la WorkPlace 
+            /// </summary>
+            /// <param name="codiceWP"></param>
+            /// <returns></returns>
+            internal static string GetCodiceWS_byCodiceWP(string codiceWP)
+            {
+                log.Debug($"GetCodiceWS_byCodiceWP(\"{codiceWP}\")");
+                using (var context = new BusinessService_DBEntities())
+                {
+                    //var clientIdParameter = new SqlParameter("@ClientId", 4);
+
+                    //var result = context.Database
+                    //    .SqlQuery<ResultForCampaign>("GetResultsForCampaign @ClientId", clientIdParameter)
+                    //    .ToList();
+                    var WpWs = context.Database
+                        .SqlQuery<WpWsDto>("WpWs")
+                        //.ToList()
+                        .Where(w => w.Workplace == codiceWP)
+                        .FirstOrDefault();
+
+                    return WpWs.CodiceStazione;
+                }
+            }
+
+
 
 
             internal static List<A_Stazioni> GetStazioni(A_TrattiFrontEnd tratto)
@@ -60,31 +88,38 @@ namespace MOM.WebInterface.App_DB
 
                 using (var context = new UteDigitaleEntities())
                 {
+                    string CodiceFirstWorkStation = GetCodiceWS_byCodiceWP(tratto.FirstWorkplace);
+
                     IOrderedQueryable<A_Stazioni> query;
 
                     if (string.IsNullOrEmpty( tratto.FirstNotIncludedWorkPlace))
                     {
+
                         query = from s in context.A_Stazioni
                                 where s.Cancellato == 0
                                 && s.Codice.Contains(tratto.Code)
-                                && s.Codice.CompareTo(tratto.FirstWorkplace) >= 0
+                                && s.Codice.CompareTo(CodiceFirstWorkStation) >= 0 // s.Codice >= CodiceFirstWorkStation
                                 orderby s.Ordine ascending
                                 select s;
                     }
                     else
                     {
+                        string CodiceFirstNotIncludedWorkStation = GetCodiceWS_byCodiceWP(tratto.FirstNotIncludedWorkPlace);
+
                         query = from s in context.A_Stazioni
                                 where s.Cancellato == 0
                                 && s.Codice.Contains(tratto.Code)
-                                && s.Codice.CompareTo(tratto.FirstWorkplace) >= 0
-                                && s.Codice.CompareTo(tratto.FirstNotIncludedWorkPlace) < 0
+                                && s.Codice.CompareTo(CodiceFirstWorkStation) >= 0 // s.Codice >= tratto.FirstWorkplace
+                                && s.Codice.CompareTo(CodiceFirstNotIncludedWorkStation) < 0  // s.Codice < CodiceFirstNotIncludedWorkStation
                                 orderby s.Ordine ascending
                                 select s;
                     }
 
-                    List<A_Stazioni> result = query.Distinct().ToList();
+                    //List<A_Stazioni> result = query.Distinct().ToList().Take(tratto.CountStazioni);
 
-                    return result;
+                    var result = query.Distinct().ToList().Take(tratto.CountStazioni);
+
+                    return result.ToList<A_Stazioni>();
                 }
             }
 
